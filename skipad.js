a78297f9a33a06fc48190f3984877fa368b37147
Shreya Dahal <shreyadahal@gmail.com>
2017-03-06 10:20:17 +0545

    Intial code commit

---
+++ skipad.js
@@ -0,0 +1,36 @@
+;(function() {
+
+  /**
+   * This function checks if the current page has a skip ad button
+   * available yet. If it has the button, a click event is fired on
+   * the button. If no such button is found, or if the current page
+   * is not a video page (/watch), then we do nothing.
+   * The interval period of 2 seconds is arbitary, but I guess 2sec
+   * is a good choice.
+   */
+  var timeout = setInterval(function() {
+    if (window.location.pathname !== '/watch') {
+      return;
+    }
+    var skipButton = document.querySelector('button.videoAdUiSkipButton');
+    if (skipButton) {
+      eventFire(skipButton, 'click');
+    }
+  }, 2000);
+
+	/**
+   * Got this function from:
+	 * http://stackoverflow.com/questions/2705583/how-to-simulate-a-click-with-javascript
+	 */
+  function eventFire(el, etype){
+    if (el.fireEvent) {
+      el.fireEvent('on' + etype);
+    } else {
+      var evObj = document.createEvent('Events');
+      evObj.initEvent(etype, true, false);
+      el.dispatchEvent(evObj);
+    }
+  }
+
+})();
+
