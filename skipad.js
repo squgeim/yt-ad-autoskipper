1fb236461c0ebe07f01b70a8e3e5c0b5e02637f9
Shreya Dahal <shreyadahal@gmail.com>
2019-10-14 18:58:10 -0400

    Switch between polling and observer implementation as required

--- skipad.js
+++ skipad.js
@@ -53,45 +53,32 @@
   /**
    * Initializes an observer on the YouTube Video Player to get events when any
    * element in there changes. We can check for the skip ad buttons then.
+   *
+   * @returns {Boolean} - true if observer could be set up, false otherwise
    */
   function initObserver() {
+    if (!('MutationObserver' in window)) {
+      return false;
+    }
+
     var ytdPlayer = (function(nodeList) {
       return nodeList && nodeList[0];
     })(document.getElementsByTagName('ytd-player'));
 
-    console.log(ytdPlayer);
-
     if (!ytdPlayer) {
-      console.warn('Can not find the YouTube player to observer. Falling back to polling implementation.');
-
-      initTimeout();
-
-      return;
+      return false;
     }
 
     var observer = new MutationObserver(function() {
-      console.log('mutation!');
       checkAndClickButtons();
     });
 
     observer.observe(ytdPlayer, { childList: true, subtree: true });
+
+    clearTimeout(timeoutId); // Just for good measure
+
+    return true;
   } 
-
-  /**
-   * Starts the poll to see if any of the ad buttons are present in the page now.
-   * 
-   * The interval of 2 seconds is arbitrary. I guess it's a good compromise.
-   */
-  function initTimeout() {
-    clearTimeout(timeoutId);
-
-    timeoutId = setTimeout(function() {
-      console.log('poll on!');
-      checkAndClickButtons();
-
-      initTimeout();
-    }, 2000);
-  }
 
   /**
    * We have two implementations to check for the skip ad buttons: one is based on
@@ -99,15 +86,29 @@
    * the page; second is a simple poll that constantly checks for the existence of
    * the skip ad buttons.
    * 
-   * We decide which implementation to start based on if the browser supports
-   * MutationObserver
+   * We first try to set up the mutation observer. It can sometimes fail even if the
+   * browser supports it if the video player has not yet been attached to the DOM.
+   * In that case, we continue the polling implementation until the observer can be
+   * set up.
    */
-  if ('MutationObserver' in window) {
-    // Initializing the observer in the next cycle to make sure the YouTube player
-    // has been added to the page.
-    setTimeout(initObserver);
-  } else {
-    // Start polling:
-    initTimeout();
+  function initTimeout() {
+    clearTimeout(timeoutId);
+
+    if (initObserver()) {
+      // We can stop the polling as the observer is set up.
+      return;
+    }
+
+    /**
+     * Starts the poll to see if any of the ad buttons are present in the page now.
+     * The interval of 2 seconds is arbitrary. I guess it's a good compromise.
+     */
+    timeoutId = setTimeout(function() {
+      checkAndClickButtons();
+
+      initTimeout();
+    }, 2000);
   }
+
+  initTimeout();
 })();
