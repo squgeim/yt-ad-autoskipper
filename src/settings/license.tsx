8b5c75c6921c05d701b259613beaab83bf082fbc
squgeim <shreyadahal@gmail.com>
2022-01-26 21:31:24 -0500

    feat: subscription prompt message

--- src/settings/license.tsx
+++ src/settings/license.tsx
@@ -10,22 +10,39 @@ const BASE_URL =
 export function License({ user }: { user: AuthUser | null }): Element {
   return (
     <section class="license">
-      <h1>{["Hi", user?.displayName?.split(" ")[0]].join(" ").trim()}!</h1>
+      <h1>{["Hi", user?.displayName?.split(" ")[0]].join(" ").trim()} 👋</h1>
       {!user?.email && (
         <>
           <p>
-            I am a software developer. 2 cups of coffee ☕️ per year to unlock
-            channel-level configuration.
+            I am a software developer from Nepal, currently living in Toronto.
+          </p>
+          <p>
+            I created YouTube Ad Auto-skipper in 2018 to make my life easier. I
+            have been maintaining it during my weekends and evenings while
+            juggling a job, school and family. I plan to focus on developing
+            much requested functionalities in the near future.
+          </p>
+          <p>
+            I only request 3 cups of coffee ☕️ per year to support the
+            development of this extension. Your support will unlock new features
+            such as muting ads and configuring ad playback length that can also
+            be configured for your favorite YouTubers.
+          </p>
+          <p>
+            The original functionality of automatically skipping ads will always
+            remain free.
           </p>
           <div class="license-btns">
             <a class="btn btn-primary" href={`${BASE_URL}/login.html?signup=1`}>
-              Sign me up for $7/yr!
+              Sign me up for $7 per year
             </a>
+            <p>
+              Are you already a supporter?{" "}
+              <a href={`${BASE_URL}/login.html`}>
+                Click here to unlock all features.
+              </a>
+            </p>
           </div>
-          <p>
-            Do you already have a subscription?{" "}
-            <a href={`${BASE_URL}/login.html`}>Apply it on this browser.</a>
-          </p>
         </>
       )}
     </section>
