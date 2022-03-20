196dfb741d78c8392e9b38158f088afbfc900817
squgeim <shreyadahal@gmail.com>
2022-03-20 01:10:54 -0400

    fix: support null in deepmerge

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -8,16 +8,20 @@ export function mainLoop(cb: () => Promise<void>, ms: number): void {
   call();
 }
 
+function isObject(o: unknown): o is object {
+  return typeof o === "object" && o !== null;
+}
+
 export function deepmerge<T extends Record<string, unknown>>(
   ...objs: unknown[]
 ): T {
   let newObj: Record<string, unknown> = {};
 
   for (const obj of objs) {
-    if (typeof obj !== "object" || obj === null) continue;
+    if (!isObject(obj)) continue;
 
     for (const [key, val] of Object.entries(obj)) {
-      if (typeof val === "object" && key in newObj) {
+      if (isObject(val) && key in newObj) {
         newObj = {
           ...newObj,
           [key]: deepmerge(val, newObj[key]),
