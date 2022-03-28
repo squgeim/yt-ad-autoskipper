9d8ba0452bf0e963f70c6271f3730ab749af5e4c
squgeim <shreyadahal@gmail.com>
2022-03-27 23:58:07 -0400

    feat: button to end skip ad countdown

--- src/contentScripts/youtube/videoAdSkipper.ts
+++ src/contentScripts/youtube/videoAdSkipper.ts
@@ -32,9 +32,55 @@ export class VideoAdSkipper implements EventHandler {
 
     this.#skipAt = addMilliseconds(new Date(), skipAdTime * 1000);
     logger.debug("scheduling skip at ", this.#skipAt);
+    this.renderCountdown();
+  }
+
+  private renderCountdown() {
+    const countdown = document.querySelector("#yas_countdown");
+    const text = countdown?.querySelector("span");
+
+    if (!this.#skipAt) {
+      countdown?.remove();
+
+      return;
+    }
+
+    const time = Math.floor(
+      Math.max(0, (this.#skipAt.getTime() - new Date().getTime()) / 1000)
+    );
+
+    const newText = document.createElement("span");
+    newText.textContent = `Ad will be skipped in ${time} seconds. `;
+
+    if (countdown) {
+      text?.replaceWith(newText);
+
+      return;
+    }
+
+    const newCountdown = document.createElement("div");
+    newCountdown.id = "yas_countdown";
+    newCountdown.style.margin = "5px 0";
+    const link = document.createElement("a");
+    link.href = "#";
+    link.style.color = "inherit";
+    link.textContent = "Click here to not skip this ad.";
+    link.onclick = () => {
+      logger.debug("skipping ad skipping.");
+      this.#skipAt = null;
+
+      return false;
+    };
+    newCountdown.append(newText, link);
+
+    document
+      .querySelector("ytd-video-primary-info-renderer #container")
+      ?.insertAdjacentElement("afterbegin", newCountdown);
   }
 
   private tick(): void {
+    this.renderCountdown();
+
     if (!this.#skipAt) {
       return;
     }
