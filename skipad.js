a478cb7c67e40a2a825e2652f487398fae016600
Shreya Dahal <shreyadahal@gmail.com>
2023-11-12 17:01:31 -0700

    patch: Include modern skip button

--- skipad.js
+++ skipad.js
@@ -3,6 +3,7 @@
     'videoAdUiSkipButton', // Old close ad button
     'ytp-ad-skip-button ytp-button', // New close ad button
     'ytp-ad-overlay-close-button', // Close overlay button
+    'ytp-ad-skip-button-modern ytp-button', // Modern close ad button
   ];
 
   var timeoutId;
